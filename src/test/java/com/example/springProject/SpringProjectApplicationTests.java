fb15a39171a6d00cb64a2725145285601aead3e8
deeptesh-rout <153610471+deeptesh-rout@users.noreply.github.com>
2024-08-16 20:41:53 +0530

    first commit

---
+++ src/test/java/com/example/springProject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
